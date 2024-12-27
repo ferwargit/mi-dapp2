1c049df37485f23ca101d4cc64d73537b81448f3
Fernando Warno <ferwar@hotmail.com>
2024-12-27 02:48:13 -0300

    modo oscuro / claro

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 // index.js
-// import { App } from './app/App.js';
-// import { MetamaskService } from './services/MetamaskService.js';
-// import { UIController } from './ui/UIController.js';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     try {
-//         console.log('DOMContentLoaded event listener triggered');
-//         const metamaskService = new MetamaskService();
-//         console.log('MetamaskService instance created');
-//         const uiController = new UIController();
-//         console.log('UIController instance created');
-//         new App(metamaskService, uiController);
-//         console.log('App instance created');
-//     } catch (error) {
-//         console.error('Error in DOMContentLoaded listener:', error);
-//     }
-// });
-
-// index.js
 import { App } from './app/App.js';
 import { MetamaskService } from './services/MetamaskService.js';
 import { UIController } from './ui/UIController.js';
